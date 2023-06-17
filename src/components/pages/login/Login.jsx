e12d078b6066f11f4a72d7fc892ea72047c0adb6
JussVi <osadze_denis_007@mail.ru>
2023-06-17 17:17:56 +0600

    remote formn

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -14,6 +14,8 @@ const Login = () => {
         <p className="login-form-header">ИНН</p>
         <input type="text" placeholder="1234567890123" className="login-form-field" />
         <p className="login-form-subheader">Не удалось найти этот ИНН</p>
+        <button className="login-form-button">Войти</button>
+        <a href="" className="login-form-link">Не удается войти?</a>
       </div>
     </div>
   );
