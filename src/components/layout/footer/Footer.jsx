e0784d37ae281016ddf6491c612ccc0a2a19ef4a
JussVi <osadze_denis_007@mail.ru>
2023-06-17 17:47:50 +0600

    add footer and header

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './style.css';
 
 const Footer = () => {
   return (
-    <div>
-      footer
-      asd
+    <div className="footer-wrap">
+      <div className="container">
+        <span className="footer-logo"></span>
+        <p className="footer-text">Портал получения пособий</p>
+        <p className="footer-info">2023 (c)  Front Power</p>
+      </div>
     </div>
   );
 }
--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -5,11 +5,8 @@ const Login = () => {
   return (
     <div className="container">
       <h1 className="login-header">
-        Портал получения пособий
-      </h1>
-      <h2 className="login-subheader">
         Введите ваш ИНН и вставьте флешку из ЦОНа
-      </h2>
+      </h1>
       <div className="login-form-block">
         <p className="login-form-header">ИНН</p>
         <input type="text" placeholder="1234567890123" className="login-form-field" />
